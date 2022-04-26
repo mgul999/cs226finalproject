771a82391761df4353a4b448204ce6f1ca6d4921
jordanbarkin <jordanbarkin@me.com>
2022-04-26 00:25:56 -0400

    started separate files

---
+++ run_experiments.py
@@ -0,0 +1,128 @@
+import numpy as np
+import math
+from scipy.optimize import linprog
+from abc import ABC
+from collections import defaultdict
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+import pulp as plt
+from mechanisms import *
+from utils import *
+
+_default_dn_args = {"epsilon" : 0.5, "should_log" : False}
+
+# Run a set of experiments. See usage below.
+def run_experiment(ns,
+                   mechanism_class,
+                   mech_args,
+                   dn_epsilon,
+                   t_generators,
+                   should_log=True,
+                   output_filename = None,
+                   attacker=dinur_nissim,
+                   data_maker = generate_bit_data,
+                   attacker_args=_default_dn_args):
+
+    results_df =  pd.DataFrame(columns=["n",
+                              *list(mech_args[0].keys()),
+                              "dn_epsilon",
+                              "num_queries",
+                              "percent_reconstructed",
+                              "mechanism_error"])
+
+    def make_entry(mech_arg, n, dn_epsilon, t, pc_reconstructed, mechanism_error):
+        res = {x : [mech_arg[x]] for x in mech_arg.keys()}
+        res.update (
+            {
+                "n" : [n],
+                "dn_epsilon" : [dn_epsilon],
+                "num_queries" : [t],
+                "percent_reconstructed" : pc_reconstructed,
+                "mechanism_error" : mechanism_error
+            }
+        )
+        return pd.DataFrame.from_dict(res)
+
+    total = len(ns)*len(mech_args)*len(t_generators)
+
+    if(should_log):
+        print(f"Running {total} experiments")
+
+    so_far = 0
+    for n in ns:
+        for mech_arg in mech_args:
+            # New dataset and mech for each size/mechanism params pair
+            data = data_maker(n=n)
+            mech = mechanism_class(data, **mech_arg)
+            mech_acc = discover_accuracy(mech)
+            for t_generator in t_generators:
+                attacker_args["epsilon"] = dn_epsilon
+                result = attacker(mech, t_generator, **attacker_args)
+                # Lambdas are hard to log so we log its application. We can probably abstract this better.
+                t = int(t_generator(n))
+
+                # Append new results
+                entry = make_entry(mech_arg, n, dn_epsilon, t, percent_reconstructed(data, result), mech_acc)
+                results_df = pd.concat([results_df, entry], ignore_index=True)
+                so_far += 1
+                if should_log:
+                    print(f"Finished {so_far}/{total}")
+
+    if output_filename:
+        if should_log:
+            print(f"Writing results to {output_filename}")
+        results_df.to_csv(output_filename, encoding='utf-8', header='true')
+    return results_df
+
+def read_results(filename):
+    "Read results written by the function above"
+    return pd.read_csv (filename, index_col=0)
+
+# General plotting functions
+def plot_3d(df, filter_cond, x1, x2, y, labels, title):
+    """
+    Make a 3d surface plot with y as a function of x1, x2.
+    x1,x2,y should be df series / arrays
+    """
+    df = filter_cond(df)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_trisurf(df[x1], df[x2], df[y], cmap=cm.jet, linewidth=0.2)
+    ax.set_xlabel(labels[0], fontsize=10)
+    ax.set_ylabel(labels[1], fontsize=10)
+    ax.set_zlabel(labels[2], fontsize=10)
+    ax.set_title(title, fontsize=15)
+    plt.show()
+    return ax
+
+def plot_single_x_multiple_subsets(df,
+                                   filter_cond,
+                                   x,
+                                   y,
+                                   divide_on,
+                                   dividers = None,
+                                   xlabel = "x",
+                                   ylabel = "y",
+                                   legend_formatter = lambda n : n,
+                                   title = "My graph"):
+    """
+    Todo: Explain this
+    """
+    fig = plt.figure()
+    ax = plt.gca()
+    if divide_on is None:
+        s = df
+        ax.plot(s[x], s[y])
+    else:
+        for div in dividers:
+            s = df[df[divide_on] == div]
+            ax.plot(s[x], s[y], label = legend_formatter(div))
+        ax.legend()
+    ax.set_title(title, fontsize=15)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return ax
+
---
+++ visualization.py
@@ -0,0 +1,107 @@
+import numpy as np
+import math
+from scipy.optimize import linprog
+from abc import ABC
+from collections import defaultdict
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+import pulp as plt
+from mechanisms import *
+from utils import *
+from run_experiments import *
+
+# Some preliminary specific plots
+
+def plot_3d_eps_t(df, title = "% Recon vs. T and Epsilon"):
+
+    plot_3d(df,
+            filter_cond = lambda x : x,
+            x1 = "num_queries",
+            x2 = "epsilon",
+            y = "percent_reconstructed",
+            labels = ['Num Queries', 'Epsilon', 'Proportion Reconstructed'],
+            title = title)
+
+
+def plot_eps_multiple_t(df, ts, title="Success vs. Eps for various Ts"):
+    plot_single_x_multiple_subsets(
+        df=df,
+        filter_cond = lambda x : x,
+        x="epsilon",
+        y="percent_reconstructed",
+        divide_on="num_queries",
+        dividers=ts,
+        xlabel = "Epsilon",
+        ylabel = "% Reconstructed",
+        legend_formatter = lambda x : f"{x} queries",
+        title = title
+    )
+
+def plot_t_multiple_eps(df, epss, title="Success vs. T for various Epsilons"):
+    plot_single_x_multiple_subsets(
+        df=df,
+        filter_cond = lambda x : x,
+        x="num_queries",
+        y="percent_reconstructed",
+        divide_on="epsilon",
+        dividers=epss,
+        xlabel = "# Queries",
+        ylabel = "% Reconstructed",
+        legend_formatter = lambda x : f"Epsilon = {x}",
+        title = title
+    )
+
+
+def epsilon_thresshold(df, threshold = 0.95, title ="Queries vs. Epsilon"):
+    """
+    Plot # queries required to attain a propoertion reconstructed >- thresshold
+    """
+    tres = df[df.percent_reconstructed >= threshold].groupby(df.epsilon).apply(lambda d : d[d.num_queries == d.num_queries.min()])
+
+    return plot_single_x_multiple_subsets(
+        df=tres,
+        filter_cond = lambda x : x,
+        x="epsilon",
+        y="num_queries",
+        divide_on=None,
+        xlabel = "Epsilon",
+        ylabel = "Queries Required",
+        title = title
+    )
+
+def recon_accuracy_tradeoff(df, title ="Accuracy and Percent Recon vs. Epsilon"):
+    """
+    Makes two plots:
+
+    Both have epsilon on the x axis.
+    First has mechanism error ("accuracy" proxy) on y axis
+    Second has % reconstructed.
+
+    Tweak the accuracy metric defined in mechanisms.py to improve this.
+    """
+    acc = plot_single_x_multiple_subsets(
+        df=df,
+        filter_cond = lambda x : x,
+        x="epsilon",
+        y="mechanism_error",
+        divide_on = None,
+        xlabel = "Epsilon",
+        ylabel = "Mechanism Error",
+        legend_formatter = lambda x : f"Epsilon = {x}",
+        title = title
+    )
+
+    perc_rec = plot_single_x_multiple_subsets(
+        df=df,
+        filter_cond = lambda x : x,
+        x="epsilon",
+        y="percent_reconstructed",
+        divide_on = None,
+        xlabel = "Epsilon",
+        ylabel = "% Reconstructed",
+        legend_formatter = lambda x : f"Epsilon = {x}",
+        title = ""
+    )
